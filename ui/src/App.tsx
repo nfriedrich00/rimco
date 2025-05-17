50324e373b18608474734a572817ffed62b347e7
Nils Friedrich <nils-jonathan.friedrich@student.tu-freiberg.de>
2025-05-17 16:50:47 +0200

    basic test to modify the page and get familiar with the tsx file

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Sandbox from "./Sandbox"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +29,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+    <main className="min-h-screen flex items-center justify-center bg-gray-50">
+      <Sandbox />
+    </main>
     </>
   )
 }
---
+++ ui/src/Sandbox.tsx
@@ -0,0 +1,9 @@
+// src/Sandbox.tsx
+export default function Sandbox() {
+  return (
+    <div className="rounded-lg shadow-md bg-white p-4">
+      <h1 className="text-brand text-xl font-bold">Tailwind v4 ✅</h1>
+    </div>
+  );
+}
+
